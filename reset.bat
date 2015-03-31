fa8d200548adda5be9cbceeee6fb0835824d13eb
Brian Bugert <coolbots7@gmail.com>
2015-03-31 00:31:07 -0700

    Initial Commit reset.bat
    Moves images back to main dir and clears output folder

---
+++ reset.bat
@@ -0,0 +1,2 @@
+move /y C:\Users\Coolbots7\Documents\GitHub\SUAS-ADLC-2015\Completed\*.* C:\Users\Coolbots7\Documents\GitHub\SUAS-ADLC-2015
+del /F /Q C:\Users\Coolbots7\Documents\GitHub\SUAS-ADLC-2015\Output\*.*
