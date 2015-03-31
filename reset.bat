b9c7c857f61235707feb9d734c31db2b5858ad1c
Brian Bugert <coolbots7@gmail.com>
2015-03-31 14:29:13 -0700

    reset.bat
    added clearing of Targets folder

--- reset.bat
+++ reset.bat
@@ -1,2 +1,3 @@
 move /y C:\Users\Coolbots7\Documents\GitHub\SUAS-ADLC-2015\Completed\*.* C:\Users\Coolbots7\Documents\GitHub\SUAS-ADLC-2015
 del /F /Q C:\Users\Coolbots7\Documents\GitHub\SUAS-ADLC-2015\Output\*.*
+del /F /Q C:\Users\Coolbots7\Documents\GitHub\SUAS-ADLC-2015\Targets\*.*
